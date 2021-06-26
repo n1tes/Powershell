97baef07386d321054f6073baf1d4c4fd8da1c6d
n1tes <57507477+n1tes@users.noreply.github.com>
2021-06-26 21:26:11 +1000

    Add files via upload

---
+++ Inventory1.ps1
@@ -0,0 +1,47 @@
+﻿Function Get-OSinfo {
+    [Cmdletbinding()]
+    param(
+        [Parameter(Mandatory=$True,
+                    ValueFromPipeline=$True,
+                    ValueFromPipelineByPropertyName=$True,
+                    HelpMessage="The. Computer. Name.")]
+        [string[]]$computername
+    )
+    BEGIN {} ##Begin, process & End block is only required for Valuefrom Pipeline property
+    ## otherwise it only outputs the last output in pipeline
+    PROCESS {
+    foreach ($computer in $computername)
+    {
+        try {
+            $cimsession = New-CimSession -ComputerName $computer -ErrorAction stop
+            $os = Get-CimInstance -ClassName Win32_OperatingSystem -CimSession $session
+            $cs = Get-CimInstance -ClassName Win32_ComputerSystem -CimSession $session
+            $properties = @{
+                            ComputerName = $computer
+                            Status = 'Connected'
+                            SPVersion = $os.ServicePackMajorVersion
+                            OSVersion = $os.Version
+                            Model = $cs.Model
+                            mfgr = $cs.Manufacturer
+                            }
+            } catch {
+            Write-Verbose "couldn't connect $computer"
+            $properties = @{
+                            ComputerName = $computer
+                            Status = 'Disconnected'
+                            SPVersion = $null
+                            OSVersion = $null
+                            Model = $null
+                            mfgr = $null
+                            }
+        
+                    } Finally {
+                    $obj = [pscustomobject]$properties
+                    Write-Output $obj
+    
+                              }
+
+    }
+    }
+    END {}
+}
