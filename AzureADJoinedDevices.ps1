73065af87a0058bf962ddad30eaf4f51b3e19ee5
n1tes <57507477+n1tes@users.noreply.github.com>
2021-07-04 14:49:35 +1000

    Add files via upload

---
+++ AzureADJoinedDevices.ps1
@@ -0,0 +1,28 @@
+﻿Connect-AzureAD
+$Result=@()
+$Users = Get-AzureADUser -All $true | Select UserPrincipalName,ObjectId, displayname, state
+$Users | ForEach-Object {
+$user = $_
+
+Get-AzureADUserRegisteredDevice -ObjectId $user.ObjectId | ForEach-Object {
+    if ($_.approximateLastlogontimestamp -gt (get-date).AddDays(-60))
+{
+    $Result += New-Object PSObject -property @{ 
+        'Item Name' = $_.DisplayName
+        Category = $null
+        'Serial Number' = $null
+        Model = $null
+        Warranty = $null
+        'Purchase Date' = $null
+        'Purchase Cost' = $null
+        Email = $user.UserPrincipalName
+        'Full Name' = $user.displayname
+        Location = $user.state
+        DeviceOSType = $_.DeviceOSType
+        ApproximateLastLogonTimeStamp = $_.ApproximateLastLogonTimeStamp
+        Status = $null
+        Notes = $null}
+}
+}
+}
+$Result | Export-Csv D:\scriptOutput\AzureADJoinedDevices1.csv
